a955be5d469036e249064e6d633357e749e5148a
kumaresan kandasamy <kumaresank2000@gmail.com>
2020-04-30 22:48:23 +0530

    update fizzbuzz

--- CoC/FizzBuzz.java
+++ CoC/FizzBuzz.java
@@ -8,15 +8,20 @@ class FizzBuzz
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number");
         int n = sc.nextInt();
-        for(int i = 1;i <= n;i++){
-            if(i%3 == 0){
+        for(int i = 1;i <= n;i++)
+        {
+            if(i%3 == 0)
+            {
                 System.out.println("fizz");
-            }else if(i%5 == 0){
+            }
+            else if(i%5 == 0)
+            {
                 System.out.println("buzz");
             }
             else if(i%3 == 0 && i%5 == 0){
                 System.out.println("fizzbuzz");
-            }else{
+            }else
+            {
                 System.out.println(i);
             }
         }
